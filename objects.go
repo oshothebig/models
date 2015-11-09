9ad719f28b708e89c4bedf47e3afc04a4fd6d2a8
Vikram Ragukumar <vragukumar@snaproute.com>
2015-11-09 12:35:23 -0800

    Add Asicd objects - first pass for SVI demo

--- objects.go
+++ objects.go
@@ -40,11 +40,6 @@ func (obj *IPV4Route) UnmarshalHTTP(r *http.Request) error {
 	return nil
 }
 
-type Vlan struct {
-	VlanId  int
-	PortMap  string
-}
-
 func (obj *Vlan) UnmarshalHTTP(r *http.Request) error {
 	return nil
 }
@@ -102,3 +97,75 @@ type NeighborState struct {
 	Messages Messages
 	Queues Queues
 }
+
+/* Start - Asicd objects */
+type Vlan struct {
+    VlanId      int
+    PortMap     string
+    PortTagType string
+}
+
+/* FIXME : RouterIf needs to be replaced by generic
+ * layer 2 object name e.x Port-21 or Vlan-5 etc.
+ * Internally this l2 object name can be translated
+ * into appropriate key.
+ */
+
+type IPv4Intf {
+    IpAddr   string
+    RouterIf int32
+}
+
+type IPv4Neighbor struct {
+    IpAddr   string
+    MacAddr  string
+    VlanId   int32
+    RouterIf int32
+}
+
+func (obj *Vlan) UnmarshalHTTP(r *http.Request) error {
+	var vlanobj Vlan
+	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
+	if err != nil {
+	 return err
+	}
+	if err := r.Body.Close(); err != nil {
+		return err
+	}
+	if err = json.Unmarshal(body, &vlanobj); err != nil  {
+		fmt.Println("# Vlan create called, unmarshal failed", vlanobj)
+	}
+	return nil
+}
+
+func (obj *IPv4Intf) UnmarshalHTTP(r *http.Request) error {
+	var v4Intf IPv4Intf
+	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
+	if err != nil {
+	 return err
+	}
+	if err := r.Body.Close(); err != nil {
+		return err
+	}
+	if err = json.Unmarshal(body, &v4Intf); err != nil  {
+		fmt.Println("# IPv4Intf create called, unmarshal failed", v4Intf)
+	}
+	return nil
+}
+
+func (obj *IPv4Neighbor) UnmarshalHTTP(r *http.Request) error {
+	var v4Nbr v4Nbr
+	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
+	if err != nil {
+	 return err
+	}
+	if err := r.Body.Close(); err != nil {
+		return err
+	}
+	if err = json.Unmarshal(body, &v4Nbr); err != nil  {
+		fmt.Println("# IPv4Neighbor create called, unmarshal failed", v4Nbr)
+	}
+	return nil
+}
+
+/* End - Asicd objects*/
