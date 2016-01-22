200ba200d06b108135cb184adca46ab03a622725
Harsha Kovuru <hkovuru@snaproute.com>
2016-01-21 23:58:40 -0800

    Add BGP timers support.

--- bgpddbthriftutil.go
+++ bgpddbthriftutil.go
@@ -17,24 +17,30 @@ func ConvertThriftTobgpdBGPGlobalConfigObj(thriftobj *bgpd.BGPGlobalConfig, dbob
 
 func ConvertbgpdBGPNeighborConfigObjToThrift(dbobj *BGPNeighborConfig, thriftobj *bgpd.BGPNeighborConfig) {
 	thriftobj.RouteReflectorClusterId = int32(dbobj.RouteReflectorClusterId)
-	thriftobj.MultiHopEnable = bool(dbobj.MultiHopEnable)
 	thriftobj.RouteReflectorClient = bool(dbobj.RouteReflectorClient)
 	thriftobj.Description = string(dbobj.Description)
 	thriftobj.MultiHopTTL = int8(dbobj.MultiHopTTL)
-	thriftobj.NeighborAddress = string(dbobj.NeighborAddress)
 	thriftobj.PeerAS = int32(dbobj.PeerAS)
-	thriftobj.LocalAS = int32(dbobj.LocalAS)
+	thriftobj.KeepaliveTime = int32(dbobj.KeepaliveTime)
 	thriftobj.AuthPassword = string(dbobj.AuthPassword)
+	thriftobj.MultiHopEnable = bool(dbobj.MultiHopEnable)
+	thriftobj.NeighborAddress = string(dbobj.NeighborAddress)
+	thriftobj.HoldTime = int32(dbobj.HoldTime)
+	thriftobj.LocalAS = int32(dbobj.LocalAS)
+	thriftobj.ConnectRetryTime = int32(dbobj.ConnectRetryTime)
 }
 
 func ConvertThriftTobgpdBGPNeighborConfigObj(thriftobj *bgpd.BGPNeighborConfig, dbobj *BGPNeighborConfig) {
 	dbobj.RouteReflectorClusterId = uint32(thriftobj.RouteReflectorClusterId)
-	dbobj.MultiHopEnable = bool(thriftobj.MultiHopEnable)
 	dbobj.RouteReflectorClient = bool(thriftobj.RouteReflectorClient)
 	dbobj.Description = string(thriftobj.Description)
 	dbobj.MultiHopTTL = uint8(thriftobj.MultiHopTTL)
-	dbobj.NeighborAddress = string(thriftobj.NeighborAddress)
 	dbobj.PeerAS = uint32(thriftobj.PeerAS)
-	dbobj.LocalAS = uint32(thriftobj.LocalAS)
+	dbobj.KeepaliveTime = uint32(thriftobj.KeepaliveTime)
 	dbobj.AuthPassword = string(thriftobj.AuthPassword)
+	dbobj.MultiHopEnable = bool(thriftobj.MultiHopEnable)
+	dbobj.NeighborAddress = string(thriftobj.NeighborAddress)
+	dbobj.HoldTime = uint32(thriftobj.HoldTime)
+	dbobj.LocalAS = uint32(thriftobj.LocalAS)
+	dbobj.ConnectRetryTime = uint32(thriftobj.ConnectRetryTime)
 }
