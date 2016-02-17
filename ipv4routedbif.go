f1836b914f096f9996236221fff7dfe2da4ac59f
mjoshiSnaproute <mjoshi@Snaproute.com>
2016-02-17 11:23:15 -0800

    Next hop IP is also a key for the route

--- ipv4routedbif.go
+++ ipv4routedbif.go
@@ -3,9 +3,9 @@ package models
 import (
 	"database/sql"
 	"fmt"
-	"reflect"
 	"strings"
 	"utils/dbutils"
+	"reflect"
 )
 
 func (obj IPV4Route) CreateDBTable(dbHdl *sql.DB) error {
@@ -13,13 +13,13 @@ func (obj IPV4Route) CreateDBTable(dbHdl *sql.DB) error {
 		"( " +
 		"DestinationNw TEXT, " +
 		"NetworkMask TEXT, " +
-		"Cost INTEGER, " +
 		"NextHopIp TEXT, " +
+		"Cost INTEGER, " +
 		"OutgoingIntfType TEXT, " +
 		"OutgoingInterface TEXT, " +
 		"Protocol TEXT, " +
-		"PRIMARY KEY(DestinationNw, NetworkMask) " +
-		")"
+		"PRIMARY KEY(DestinationNw, NetworkMask, NextHopIp) " +
+	")"
 
 	_, err := dbutils.ExecuteSQLStmt(dbCmd, dbHdl)
 	return err
@@ -27,18 +27,18 @@ func (obj IPV4Route) CreateDBTable(dbHdl *sql.DB) error {
 
 func (obj IPV4Route) StoreObjectInDb(dbHdl *sql.DB) (int64, error) {
 	var objectId int64
-	dbCmd := fmt.Sprintf("INSERT INTO IPV4Route (DestinationNw, NetworkMask, Cost, NextHopIp, OutgoingIntfType, OutgoingInterface, Protocol) VALUES ('%v', '%v', '%v', '%v', '%v', '%v', '%v') ;",
-		obj.DestinationNw, obj.NetworkMask, obj.Cost, obj.NextHopIp, obj.OutgoingIntfType, obj.OutgoingInterface, obj.Protocol)
+	dbCmd := fmt.Sprintf("INSERT INTO IPV4Route (DestinationNw, NetworkMask, NextHopIp, Cost, OutgoingIntfType, OutgoingInterface, Protocol) VALUES ('%v', '%v', '%v', '%v', '%v', '%v', '%v') ;",
+		obj.DestinationNw, obj.NetworkMask, obj.NextHopIp, obj.Cost, obj.OutgoingIntfType, obj.OutgoingInterface, obj.Protocol)
 	fmt.Println("**** Create Object called with ", obj)
 
 	result, err := dbutils.ExecuteSQLStmt(dbCmd, dbHdl)
 	if err != nil {
 		fmt.Println("**** Failed to Create table", err)
 	} else {
-		objectId, err = result.LastInsertId()
-		if err != nil {
-			fmt.Println("### Failed to return last object id", err)
-		}
+	    objectId, err = result.LastInsertId()
+	    if err != nil {
+		    fmt.Println("### Failed to return last object id", err)
+	    }
 
 	}
 	return objectId, err
@@ -61,19 +61,19 @@ func (obj IPV4Route) GetObjectFromDb(objKey string, dbHdl *sql.DB) (ConfigObj, e
 	var object IPV4Route
 	sqlKey, err := obj.GetSqlKeyStr(objKey)
 	dbCmd := "select * from IPV4Route where " + sqlKey
-	err = dbHdl.QueryRow(dbCmd).Scan(&object.DestinationNw, &object.NetworkMask, &object.Cost, &object.NextHopIp, &object.OutgoingIntfType, &object.OutgoingInterface, &object.Protocol)
+	err = dbHdl.QueryRow(dbCmd).Scan(&object.DestinationNw, &object.NetworkMask, &object.NextHopIp, &object.Cost, &object.OutgoingIntfType, &object.OutgoingInterface, &object.Protocol, )
 	fmt.Println("### DB Get IPV4Route\n", err)
 	return object, err
 }
 
 func (obj IPV4Route) GetKey() (string, error) {
-	key := string(obj.DestinationNw) + "#" + string(obj.NetworkMask)
+	key := string(obj.DestinationNw) + "#" + string(obj.NetworkMask) + "#" + string(obj.NextHopIp)
 	return key, nil
 }
 
 func (obj IPV4Route) GetSqlKeyStr(objKey string) (string, error) {
 	keys := strings.Split(objKey, "#")
-	sqlKey := "DestinationNw = " + "\"" + keys[0] + "\"" + " and " + "NetworkMask = " + "\"" + keys[1] + "\""
+	sqlKey := "DestinationNw = "+ "\"" + keys[0] + "\"" + " and " + "NetworkMask = "+ "\"" + keys[1] + "\"" + " and " + "NextHopIp = "+ "\"" + keys[2] + "\""
 	return sqlKey, nil
 }
 
@@ -86,27 +86,27 @@ func (obj *IPV4Route) GetAllObjFromDb(dbHdl *sql.DB) (objList []*IPV4Route, e er
 	}
 
 	defer rows.Close()
-
+    
 	for rows.Next() {
 
-		object := new(IPV4Route)
-		if err = rows.Scan(&object.DestinationNw, &object.NetworkMask, &object.Cost, &object.NextHopIp, &object.OutgoingIntfType, &object.OutgoingInterface, &object.Protocol); err != nil {
+             object := new(IPV4Route)
+             if err = rows.Scan(&object.DestinationNw, &object.NetworkMask, &object.NextHopIp, &object.Cost, &object.OutgoingIntfType, &object.OutgoingInterface, &object.Protocol, ); err != nil {
 
-			fmt.Println("Db method Scan failed when interating over IPV4Route")
-		}
-		objList = append(objList, object)
-	}
-	return objList, nil
-}
-func (obj IPV4Route) CompareObjectsAndDiff(updateKeys map[string]bool, dbObj ConfigObj) ([]bool, error) {
+             fmt.Println("Db method Scan failed when interating over IPV4Route")
+             }
+	objList = append(objList, object)
+    }
+    return objList, nil
+    }
+    func (obj IPV4Route) CompareObjectsAndDiff(updateKeys map[string]bool, dbObj ConfigObj) ([]bool, error) {
 	dbV4Route := dbObj.(IPV4Route)
 	objTyp := reflect.TypeOf(obj)
 	objVal := reflect.ValueOf(obj)
 	dbObjVal := reflect.ValueOf(dbV4Route)
 	attrIds := make([]bool, objTyp.NumField())
 	idx := 0
-	for i := 0; i < objTyp.NumField(); i++ {
-		fieldTyp := objTyp.Field(i)
+	for i:=0; i<objTyp.NumField(); i++ {
+	    fieldTyp := objTyp.Field(i)
 		if fieldTyp.Anonymous {
 			continue
 		}
@@ -114,112 +114,112 @@ func (obj IPV4Route) CompareObjectsAndDiff(updateKeys map[string]bool, dbObj Con
 		objVal := objVal.Field(i)
 		dbObjVal := dbObjVal.Field(i)
 		if _, ok := updateKeys[fieldTyp.Name]; ok {
-			if objVal.Kind() == reflect.Int {
-				if int(objVal.Int()) != int(dbObjVal.Int()) {
-					attrIds[idx] = true
-				}
-			} else if objVal.Kind() == reflect.Int8 {
-				if int8(objVal.Int()) != int8(dbObjVal.Int()) {
-					attrIds[idx] = true
-				}
-			} else if objVal.Kind() == reflect.Int16 {
-				if int16(objVal.Int()) != int16(dbObjVal.Int()) {
-					attrIds[idx] = true
-				}
-			} else if objVal.Kind() == reflect.Int32 {
-				if int32(objVal.Int()) != int32(dbObjVal.Int()) {
-					attrIds[idx] = true
-				}
-			} else if objVal.Kind() == reflect.Int64 {
-				if int64(objVal.Int()) != int64(dbObjVal.Int()) {
-					attrIds[idx] = true
-				}
-			} else if objVal.Kind() == reflect.Uint {
-				if uint(objVal.Uint()) != uint(dbObjVal.Uint()) {
-					attrIds[idx] = true
-				}
-			} else if objVal.Kind() == reflect.Uint8 {
-				if uint8(objVal.Uint()) != uint8(dbObjVal.Uint()) {
-					attrIds[idx] = true
-				}
-			} else if objVal.Kind() == reflect.Uint16 {
-				if uint16(objVal.Uint()) != uint16(dbObjVal.Uint()) {
-					attrIds[idx] = true
-				}
-			} else if objVal.Kind() == reflect.Uint32 {
-				if uint16(objVal.Uint()) != uint16(dbObjVal.Uint()) {
-					attrIds[idx] = true
-				}
-			} else if objVal.Kind() == reflect.Uint64 {
-				if uint16(objVal.Uint()) != uint16(dbObjVal.Uint()) {
-					attrIds[idx] = true
-				}
-			} else if objVal.Kind() == reflect.Bool {
-				if bool(objVal.Bool()) != bool(dbObjVal.Bool()) {
-					attrIds[idx] = true
-				}
-			} else {
-				if objVal.String() != dbObjVal.String() {
-					attrIds[idx] = true
-				}
-			}
-			if attrIds[idx] {
+            if objVal.Kind() == reflect.Int {
+                if int(objVal.Int()) != int(dbObjVal.Int()) {
+                    attrIds[idx] = true
+                }
+            } else if objVal.Kind() == reflect.Int8 {
+                if int8(objVal.Int()) != int8(dbObjVal.Int()) {
+                    attrIds[idx] = true
+                }
+            } else if objVal.Kind() == reflect.Int16 {
+                if int16(objVal.Int()) != int16(dbObjVal.Int()) {
+                    attrIds[idx] = true
+                }
+            } else if objVal.Kind() == reflect.Int32 {
+                if int32(objVal.Int()) != int32(dbObjVal.Int()) {
+                    attrIds[idx] = true
+                }
+            } else if objVal.Kind() == reflect.Int64 {
+                if int64(objVal.Int()) != int64(dbObjVal.Int()) {
+                    attrIds[idx] = true
+                }
+            } else if objVal.Kind() == reflect.Uint {
+                if uint(objVal.Uint()) != uint(dbObjVal.Uint()) {
+                    attrIds[idx] = true
+                }
+            } else if objVal.Kind() == reflect.Uint8 {
+                if uint8(objVal.Uint()) != uint8(dbObjVal.Uint()) {
+                    attrIds[idx] = true
+                }
+            } else if objVal.Kind() == reflect.Uint16 {
+                if uint16(objVal.Uint()) != uint16(dbObjVal.Uint()) {
+                    attrIds[idx] = true
+                }
+            } else if objVal.Kind() == reflect.Uint32 {
+                if uint16(objVal.Uint()) != uint16(dbObjVal.Uint()) {
+                    attrIds[idx] = true
+                }
+            } else if objVal.Kind() == reflect.Uint64 {
+                if uint16(objVal.Uint()) != uint16(dbObjVal.Uint()) {
+                    attrIds[idx] = true
+                }
+            } else if objVal.Kind() == reflect.Bool {
+                if bool(objVal.Bool()) != bool(dbObjVal.Bool()) {
+                    attrIds[idx] = true
+                }
+            } else {
+                if objVal.String() != dbObjVal.String() {
+                    attrIds[idx] = true
+                }
+            }
+            if attrIds[idx] {
 				fmt.Println("attribute changed ", fieldTyp.Name)
 			}
-		}
+        }
 		idx++
 
 	}
 	return attrIds[:idx], nil
 }
 
-func (obj IPV4Route) MergeDbAndConfigObj(dbObj ConfigObj, attrSet []bool) (ConfigObj, error) {
+    func (obj IPV4Route) MergeDbAndConfigObj(dbObj ConfigObj, attrSet []bool) (ConfigObj, error) {
 	var mergedIPV4Route IPV4Route
 	objTyp := reflect.TypeOf(obj)
 	objVal := reflect.ValueOf(obj)
 	dbObjVal := reflect.ValueOf(dbObj)
 	mergedObjVal := reflect.ValueOf(&mergedIPV4Route)
 	idx := 0
-	for i := 0; i < objTyp.NumField(); i++ {
+	for i:=0; i<objTyp.NumField(); i++ {
 		if fieldTyp := objTyp.Field(i); fieldTyp.Anonymous {
 			continue
 		}
 
 		objField := objVal.Field(i)
 		dbObjField := dbObjVal.Field(i)
-		if attrSet[idx] {
+		if  attrSet[idx] {
 			if dbObjField.Kind() == reflect.Int ||
-				dbObjField.Kind() == reflect.Int8 ||
-				dbObjField.Kind() == reflect.Int16 ||
-				dbObjField.Kind() == reflect.Int32 ||
-				dbObjField.Kind() == reflect.Int64 {
+			   dbObjField.Kind() == reflect.Int8 ||
+			   dbObjField.Kind() == reflect.Int16 ||
+			   dbObjField.Kind() == reflect.Int32 ||
+			   dbObjField.Kind() == reflect.Int64 {
 				mergedObjVal.Elem().Field(i).SetInt(objField.Int())
 			} else if dbObjField.Kind() == reflect.Uint ||
-				dbObjField.Kind() == reflect.Uint8 ||
-				dbObjField.Kind() == reflect.Uint16 ||
-				dbObjField.Kind() == reflect.Uint32 ||
-				dbObjField.Kind() == reflect.Uint64 {
-				mergedObjVal.Elem().Field(i).SetUint(objField.Uint())
+			   dbObjField.Kind() == reflect.Uint8 ||
+			   dbObjField.Kind() == reflect.Uint16 ||
+			   dbObjField.Kind() == reflect.Uint32 ||
+			   dbObjField.Kind() == reflect.Uint64 {
+			    mergedObjVal.Elem().Field(i).SetUint(objField.Uint())
 			} else if dbObjField.Kind() == reflect.Bool {
-				mergedObjVal.Elem().Field(i).SetBool(objField.Bool())
+			    mergedObjVal.Elem().Field(i).SetBool(objField.Bool())
 			} else {
 				mergedObjVal.Elem().Field(i).SetString(objField.String())
 			}
 		} else {
 			if dbObjField.Kind() == reflect.Int ||
-				dbObjField.Kind() == reflect.Int8 ||
-				dbObjField.Kind() == reflect.Int16 ||
-				dbObjField.Kind() == reflect.Int32 ||
-				dbObjField.Kind() == reflect.Int64 {
+			   dbObjField.Kind() == reflect.Int8 ||
+			   dbObjField.Kind() == reflect.Int16 ||
+			   dbObjField.Kind() == reflect.Int32 ||
+			   dbObjField.Kind() == reflect.Int64 {
 				mergedObjVal.Elem().Field(i).SetInt(dbObjField.Int())
 			} else if dbObjField.Kind() == reflect.Uint ||
-				dbObjField.Kind() == reflect.Uint ||
-				dbObjField.Kind() == reflect.Uint8 ||
-				dbObjField.Kind() == reflect.Uint16 ||
-				dbObjField.Kind() == reflect.Uint32 {
-				mergedObjVal.Elem().Field(i).SetUint(dbObjField.Uint())
+			   dbObjField.Kind() == reflect.Uint ||
+			   dbObjField.Kind() == reflect.Uint8 ||
+			   dbObjField.Kind() == reflect.Uint16 ||
+			   dbObjField.Kind() == reflect.Uint32 {
+			    mergedObjVal.Elem().Field(i).SetUint(dbObjField.Uint())
 			} else if dbObjField.Kind() == reflect.Bool {
-				mergedObjVal.Elem().Field(i).SetBool(dbObjField.Bool())
+			    mergedObjVal.Elem().Field(i).SetBool(dbObjField.Bool())
 			} else {
 				mergedObjVal.Elem().Field(i).SetString(dbObjField.String())
 			}
@@ -230,16 +230,16 @@ func (obj IPV4Route) MergeDbAndConfigObj(dbObj ConfigObj, attrSet []bool) (Confi
 	return mergedIPV4Route, nil
 }
 
-func (obj IPV4Route) UpdateObjectInDb(dbObj ConfigObj, attrSet []bool, dbHdl *sql.DB) error {
+    func (obj IPV4Route) UpdateObjectInDb(dbObj ConfigObj, attrSet []bool, dbHdl *sql.DB) error {
 	var fieldSqlStr string
 	dbIPV4Route := dbObj.(IPV4Route)
 	objKey, err := dbIPV4Route.GetKey()
 	objSqlKey, err := dbIPV4Route.GetSqlKeyStr(objKey)
 	dbCmd := "update " + "IPV4Route" + " set"
-	objTyp := reflect.TypeOf(obj)
+objTyp := reflect.TypeOf(obj)
 	objVal := reflect.ValueOf(obj)
 	idx := 0
-	for i := 0; i < objTyp.NumField(); i++ {
+	for i:=0; i<objTyp.NumField(); i++ {
 		if fieldTyp := objTyp.Field(i); fieldTyp.Anonymous {
 			continue
 		}
@@ -248,19 +248,19 @@ func (obj IPV4Route) UpdateObjectInDb(dbObj ConfigObj, attrSet []bool, dbHdl *sq
 			fieldTyp := objTyp.Field(i)
 			fieldVal := objVal.Field(i)
 			if fieldVal.Kind() == reflect.Int ||
-				fieldVal.Kind() == reflect.Int8 ||
-				fieldVal.Kind() == reflect.Int16 ||
-				fieldVal.Kind() == reflect.Int32 ||
-				fieldVal.Kind() == reflect.Int64 {
+			   fieldVal.Kind() == reflect.Int8 ||
+			   fieldVal.Kind() == reflect.Int16 ||
+			   fieldVal.Kind() == reflect.Int32 ||
+			   fieldVal.Kind() == reflect.Int64 {
 				fieldSqlStr = fmt.Sprintf(" %s = '%d' ", fieldTyp.Name, int(fieldVal.Int()))
 			} else if fieldVal.Kind() == reflect.Uint ||
-				fieldVal.Kind() == reflect.Uint8 ||
-				fieldVal.Kind() == reflect.Uint16 ||
-				fieldVal.Kind() == reflect.Uint32 ||
-				fieldVal.Kind() == reflect.Uint64 {
-				fieldSqlStr = fmt.Sprintf(" %s = '%d' ", fieldTyp.Name, int(fieldVal.Uint()))
+			   fieldVal.Kind() == reflect.Uint8 ||
+			   fieldVal.Kind() == reflect.Uint16 ||
+			   fieldVal.Kind() == reflect.Uint32 ||
+			   fieldVal.Kind() == reflect.Uint64 {
+			    fieldSqlStr = fmt.Sprintf(" %s = '%d' ", fieldTyp.Name, int(fieldVal.Uint()))
 			} else if objVal.Kind() == reflect.Bool {
-				fieldSqlStr = fmt.Sprintf(" %s = '%d' ", fieldTyp.Name, dbutils.ConvertBoolToInt(bool(fieldVal.Bool())))
+			    fieldSqlStr = fmt.Sprintf(" %s = '%d' ", fieldTyp.Name, dbutils.ConvertBoolToInt(bool(fieldVal.Bool())))
 			} else {
 				fieldSqlStr = fmt.Sprintf(" %s = '%s' ", fieldTyp.Name, fieldVal.String())
 			}
