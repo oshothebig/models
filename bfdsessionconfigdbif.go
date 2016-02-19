98224c57a2171349b51a1066745c16a6c6397455
Avoy Nanda <ananda@snaproute.com>
2016-02-19 11:39:58 -0800

    Fixes

--- bfdsessionconfigdbif.go
+++ bfdsessionconfigdbif.go
@@ -255,7 +255,7 @@ func (obj BfdSessionConfig) UpdateObjectInDb(dbObj ConfigObj, attrSet []bool, db
 				fieldVal.Kind() == reflect.Uint32 ||
 				fieldVal.Kind() == reflect.Uint64 {
 				fieldSqlStr = fmt.Sprintf(" %s = '%d' ", fieldTyp.Name, int(fieldVal.Uint()))
-			} else if objVal.Kind() == reflect.Bool {
+			} else if fieldVal.Kind() == reflect.Bool {
 				fieldSqlStr = fmt.Sprintf(" %s = '%d' ", fieldTyp.Name, dbutils.ConvertBoolToInt(bool(fieldVal.Bool())))
 			} else {
 				fieldSqlStr = fmt.Sprintf(" %s = '%s' ", fieldTyp.Name, fieldVal.String())
