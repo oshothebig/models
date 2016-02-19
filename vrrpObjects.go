0a708007d06962a445eace6fd640cdffb27e1717
Jay Gheewala <jgheewala@snaproute.com>
2016-02-18 22:06:26 -0800

    build error

--- vrrpObjects.go
+++ vrrpObjects.go
@@ -8,7 +8,7 @@ type VrrpIntfConfig struct {
 	IfIndex                 int32  `SNAPROUTE: "KEY", ACCESS:"w",  MULTIPLICITY:"*"`
 	VRID                    int32  // no default for VRID
 	Priority                int32  // default value is 100
-	IPv4Addr                string // No Default for IPv4 addr.. Can support one or more IPv4 addresses
+	VirtualIPv4Addr         string // No Default for Virtual IPv4 addr.. Can support one or more
 	AdvertisementInterval   int32  // Default is 100 centiseconds which is 1 SEC
 	PreemptMode             bool   // False to prohibit preemption. Default is True.
 	AcceptMode              bool   // The default is False.
