4d93c2290c662a40e98924fe220ed753b6d15cde
Jay Gheewala <jgheewala@snaproute.com>
2016-03-07 11:06:49 -0800

    ip tables acl rule object

--- sysObjects.go
+++ sysObjects.go
@@ -13,3 +13,14 @@ type ComponentLoggingConfig struct {
 	Module string `SNAPROUTE: "KEY", ACCESS:"w",  MULTIPLICITY:"*", DESCRIPTION: "Module name to set logging level"`
 	Level  string `DESCRIPTION: "Logging level", DEFAULT: "info"`
 }
+
+type IpTableAclConfig struct {
+	BaseObj
+	Name     string `SNAPROUTE: "KEY", ACCESS:"w", MULTIPLICITY:"*", DESCRIPTION: "Ip Table ACL rule name"`
+	IfIndex  int32  `DESCRIPTION: "IfIndex where the acl rule is to be applied", DEFAULT: 0`
+	Access   string `DESCRIPTION: "permit or deny"`
+	IpAddr   string `DESCRIPTION: "ip address of subnet or host"`
+	NetMask  string `DESCRIPTION: "netmask for ip addr", DEFAULT: "0.0.0.0"`
+	Protocol string `DESCRITION: "protocol for which rule is to be applied, e.g TCP, UDP"`
+	Port     string `DESCRITION: "port for protocol"`
+}
