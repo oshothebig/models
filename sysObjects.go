3f3ee34c5a8254fd4a42adc6e44af7ebe9ff3104
Jay Gheewala <jgheewala@snaproute.com>
2016-03-10 18:18:29 -0800

    model changes for ip table acl rule

--- sysObjects.go
+++ sysObjects.go
@@ -14,12 +14,12 @@ type ComponentLoggingConfig struct {
 	Level  string `DESCRIPTION: "Logging level", DEFAULT: "info"`
 }
 
-//type IpTableAclConfig struct {
-//	BaseObj
-//	Name         string `SNAPROUTE: "KEY", ACCESS:"w", MULTIPLICITY:"*", DESCRIPTION: "Ip Table ACL rule name"`
-//	PhysicalPort string `DESCRIPTION: "IfIndex where the acl rule is to be applied", DEFAULT: "all"`
-//	Action       string `DESCRIPTION: "permit or deny"`
-//	IpAddr       string `DESCRIPTION: "ip address of subnet or host, e.g: 192.168.1.0/24, 192.168.1.1"`
-//	Protocol     string `DESCRITION: "protocol for which rule is to be applied, e.g TCP, UDP"`
-//	Port         string `DESCRITION: "port for protocol, e.g for dhcprelay port is 68", DEFAULT: "all"`
-//}
+type IpTableAclConfig struct {
+	BaseObj
+	Name         string `SNAPROUTE: "KEY", ACCESS:"w", MULTIPLICITY:"*", DESCRIPTION: "Ip Table ACL rule name"`
+	PhysicalPort string `DESCRIPTION: "IfIndex where the acl rule is to be applied", DEFAULT: "all"`
+	Action       string `DESCRIPTION: "ACCEPT or DROP"`
+	IpAddr       string `DESCRIPTION: "ip address of subnet or host, e.g: 192.168.1.0/24, 192.168.1.1"`
+	Protocol     string `DESCRITION: "protocol for which rule is to be applied, e.g TCP, UDP"`
+	Port         string `DESCRITION: "port for protocol, e.g for dhcprelay port is 68", DEFAULT: "all"`
+}
