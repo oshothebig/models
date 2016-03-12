ad5b856e7743e05130ea311a9a2d682c34d0735c
mjoshiSnaproute <mjoshi@Snaproute.com>
2016-03-12 13:38:16 -0800

    SO-400

--- policystmtconfigdbif.go
+++ policystmtconfigdbif.go
@@ -3,9 +3,9 @@ package models
 import (
 	"database/sql"
 	"fmt"
-	"reflect"
 	"strings"
 	"utils/dbutils"
+	"reflect"
 )
 
 func (obj PolicyStmtConfig) CreateDBTable(dbHdl *sql.DB) error {
@@ -13,30 +13,68 @@ func (obj PolicyStmtConfig) CreateDBTable(dbHdl *sql.DB) error {
 		"( " +
 		"Name TEXT, " +
 		"MatchConditions TEXT, " +
-		"Conditions TEXT, " +
-		"Actions TEXT, " +
 		"PRIMARY KEY(Name) " +
-		")"
-
+	")"
 	_, err := dbutils.ExecuteSQLStmt(dbCmd, dbHdl)
+	dbCmd = "CREATE TABLE IF NOT EXISTS PolicyStmtConfigConditions " +
+		"( " +
+		"ListName TEXT NOT NULL,\n" +
+		"Conditions TEXT,\n" +
+		"FOREIGN KEY(ListName) REFERENCES PolicyStmtConfig(Name) ON DELETE CASCADE" +
+		");"
+	_, err = dbutils.ExecuteSQLStmt(dbCmd, dbHdl)
+	dbCmd = "CREATE TABLE IF NOT EXISTS PolicyStmtConfigActions " +
+		"( " +
+		"ListName TEXT NOT NULL,\n" +
+		"Actions TEXT,\n" +
+		"FOREIGN KEY(ListName) REFERENCES PolicyStmtConfig(Name) ON DELETE CASCADE" +
+		");"
+	_, err = dbutils.ExecuteSQLStmt(dbCmd, dbHdl)
 	return err
 }
 
 func (obj PolicyStmtConfig) StoreObjectInDb(dbHdl *sql.DB) (int64, error) {
 	var objectId int64
-	dbCmd := fmt.Sprintf("INSERT INTO PolicyStmtConfig (Name, MatchConditions, Conditions, Actions) VALUES ('%v', '%v', '%v', '%v') ;",
-		obj.Name, obj.MatchConditions, obj.Conditions, obj.Actions)
+	var i int
+	dbCmd := fmt.Sprintf("INSERT INTO PolicyStmtConfig (Name, MatchConditions) VALUES ('%v', '%v') ;",
+		obj.Name, obj.MatchConditions)
 	fmt.Println("**** Create Object called with ", obj)
 
 	result, err := dbutils.ExecuteSQLStmt(dbCmd, dbHdl)
 	if err != nil {
 		fmt.Println("**** Failed to Create table", err)
 	} else {
-		objectId, err = result.LastInsertId()
-		if err != nil {
-			fmt.Println("### Failed to return last object id", err)
-		}
+	    objectId, err = result.LastInsertId()
+	    if err != nil {
+		    fmt.Println("### Failed to return last object id", err)
+	    }
 
+	}
+	for i = 0; i < len(obj.Conditions); i++ {
+		dbCmd = fmt.Sprintf("INSERT INTO PolicyStmtConfigConditions (ListName, Conditions) VALUES ('%v', '%v') ;",
+			obj.Name, obj.Conditions[i])
+		result, err := dbutils.ExecuteSQLStmt(dbCmd, dbHdl)
+		if err != nil {
+			fmt.Println("**** Failed to Create table", err)
+		} else {
+			_, err = result.LastInsertId()
+			if err != nil {
+				fmt.Println("### Failed to return last object id", err)
+			}
+		}
+	}
+	for i = 0; i < len(obj.Actions); i++ {
+		dbCmd = fmt.Sprintf("INSERT INTO PolicyStmtConfigActions (ListName, Actions) VALUES ('%v', '%v') ;",
+			obj.Name, obj.Actions[i])
+		result, err := dbutils.ExecuteSQLStmt(dbCmd, dbHdl)
+		if err != nil {
+			fmt.Println("**** Failed to Create table", err)
+		} else {
+			_, err = result.LastInsertId()
+			if err != nil {
+				fmt.Println("### Failed to return last object id", err)
+			}
+		}
 	}
 	return objectId, err
 }
@@ -60,32 +98,29 @@ func (obj PolicyStmtConfig) GetObjectFromDb(objKey string, dbHdl *sql.DB) (Confi
 	dbCmd := "select * from PolicyStmtConfig where " + sqlKey
 	//var tmp2 string
 	//var tmp3 string
-	err = dbHdl.QueryRow(dbCmd).Scan(&object.Name, &object.MatchConditions) //, &tmp2, &tmp3, )
+	err = dbHdl.QueryRow(dbCmd).Scan(&object.Name, &object.MatchConditions)//, &tmp2, &tmp3, )
 	fmt.Println("### DB Get PolicyStmtConfig\n", err)
-	/*convtmpConditions := strings.Split(tmp2, ",")
-	                          for _, x := range convtmpConditions {
-	                              y := strings.Replace(x, " ", "", 1)
-	                       object.Conditions = append(object.Conditions, string(y))
-	                       }
-	  convtmpActions := strings.Split(tmp3, ",")
-	                          for _, x := range convtmpActions {
-	                              y := strings.Replace(x, " ", "", 1)
-	                       object.Actions = append(object.Actions, string(y))
-	                       }*/
+/*convtmpConditions := strings.Split(tmp2, ",")
+                        for _, x := range convtmpConditions {
+                            y := strings.Replace(x, " ", "", 1)
+                     object.Conditions = append(object.Conditions, string(y))
+                     }
+convtmpActions := strings.Split(tmp3, ",")
+                        for _, x := range convtmpActions {
+                            y := strings.Replace(x, " ", "", 1)
+                     object.Actions = append(object.Actions, string(y))
+                     }*/
 	return object, err
 }
 
 func (obj PolicyStmtConfig) GetKey() (string, error) {
-	keyName := "PolicyStmtConfig"
-	keyName = strings.TrimSuffix(keyName, "Config")
-	keyName = strings.TrimSuffix(keyName, "State")
-	key := keyName + "#" + string(obj.Name)
+	key := string(obj.Name)
 	return key, nil
 }
 
 func (obj PolicyStmtConfig) GetSqlKeyStr(objKey string) (string, error) {
 	keys := strings.Split(objKey, "#")
-	sqlKey := "Name = " + "\"" + keys[1] + "\""
+	sqlKey := "Name = "+ "\"" + keys[0] + "\""
 	return sqlKey, nil
 }
 
@@ -129,8 +164,8 @@ func (obj PolicyStmtConfig) CompareObjectsAndDiff(updateKeys map[string]bool, db
 	dbObjVal := reflect.ValueOf(dbV4Route)
 	attrIds := make([]bool, objTyp.NumField())
 	idx := 0
-	for i := 0; i < objTyp.NumField(); i++ {
-		fieldTyp := objTyp.Field(i)
+	for i:=0; i<objTyp.NumField(); i++ {
+	    fieldTyp := objTyp.Field(i)
 		if fieldTyp.Anonymous {
 			continue
 		}
@@ -138,112 +173,114 @@ func (obj PolicyStmtConfig) CompareObjectsAndDiff(updateKeys map[string]bool, db
 		objVal := objVal.Field(i)
 		dbObjVal := dbObjVal.Field(i)
 		if _, ok := updateKeys[fieldTyp.Name]; ok {
-			if objVal.Kind() == reflect.Int {
-				if int(objVal.Int()) != int(dbObjVal.Int()) {
-					attrIds[idx] = true
-				}
-			} else if objVal.Kind() == reflect.Int8 {
-				if int8(objVal.Int()) != int8(dbObjVal.Int()) {
-					attrIds[idx] = true
-				}
-			} else if objVal.Kind() == reflect.Int16 {
-				if int16(objVal.Int()) != int16(dbObjVal.Int()) {
-					attrIds[idx] = true
-				}
-			} else if objVal.Kind() == reflect.Int32 {
-				if int32(objVal.Int()) != int32(dbObjVal.Int()) {
-					attrIds[idx] = true
-				}
-			} else if objVal.Kind() == reflect.Int64 {
-				if int64(objVal.Int()) != int64(dbObjVal.Int()) {
-					attrIds[idx] = true
-				}
-			} else if objVal.Kind() == reflect.Uint {
-				if uint(objVal.Uint()) != uint(dbObjVal.Uint()) {
-					attrIds[idx] = true
-				}
-			} else if objVal.Kind() == reflect.Uint8 {
-				if uint8(objVal.Uint()) != uint8(dbObjVal.Uint()) {
-					attrIds[idx] = true
-				}
-			} else if objVal.Kind() == reflect.Uint16 {
-				if uint16(objVal.Uint()) != uint16(dbObjVal.Uint()) {
-					attrIds[idx] = true
-				}
-			} else if objVal.Kind() == reflect.Uint32 {
-				if uint16(objVal.Uint()) != uint16(dbObjVal.Uint()) {
-					attrIds[idx] = true
-				}
-			} else if objVal.Kind() == reflect.Uint64 {
-				if uint16(objVal.Uint()) != uint16(dbObjVal.Uint()) {
-					attrIds[idx] = true
-				}
-			} else if objVal.Kind() == reflect.Bool {
-				if bool(objVal.Bool()) != bool(dbObjVal.Bool()) {
-					attrIds[idx] = true
-				}
-			} else {
-				if objVal.String() != dbObjVal.String() {
-					attrIds[idx] = true
-				}
-			}
-			if attrIds[idx] {
+            if objVal.Kind() == reflect.Int {
+                if int(objVal.Int()) != int(dbObjVal.Int()) {
+                    attrIds[idx] = true
+                }
+            } else if objVal.Kind() == reflect.Int8 {
+                if int8(objVal.Int()) != int8(dbObjVal.Int()) {
+                    attrIds[idx] = true
+                }
+            } else if objVal.Kind() == reflect.Int16 {
+                if int16(objVal.Int()) != int16(dbObjVal.Int()) {
+                    attrIds[idx] = true
+                }
+            } else if objVal.Kind() == reflect.Int32 {
+                if int32(objVal.Int()) != int32(dbObjVal.Int()) {
+                    attrIds[idx] = true
+                }
+            } else if objVal.Kind() == reflect.Int64 {
+                if int64(objVal.Int()) != int64(dbObjVal.Int()) {
+                    attrIds[idx] = true
+                }
+            } else if objVal.Kind() == reflect.Uint {
+                if uint(objVal.Uint()) != uint(dbObjVal.Uint()) {
+                    attrIds[idx] = true
+                }
+            } else if objVal.Kind() == reflect.Uint8 {
+                if uint8(objVal.Uint()) != uint8(dbObjVal.Uint()) {
+                    attrIds[idx] = true
+                }
+            } else if objVal.Kind() == reflect.Uint16 {
+                if uint16(objVal.Uint()) != uint16(dbObjVal.Uint()) {
+                    attrIds[idx] = true
+                }
+            } else if objVal.Kind() == reflect.Uint32 {
+                if uint16(objVal.Uint()) != uint16(dbObjVal.Uint()) {
+                    attrIds[idx] = true
+                }
+            } else if objVal.Kind() == reflect.Uint64 {
+                if uint16(objVal.Uint()) != uint16(dbObjVal.Uint()) {
+                    attrIds[idx] = true
+                }
+            } else if objVal.Kind() == reflect.Bool {
+                if bool(objVal.Bool()) != bool(dbObjVal.Bool()) {
+                    attrIds[idx] = true
+                }
+            } else {
+                if objVal.String() != dbObjVal.String() {
+                    attrIds[idx] = true
+                }
+            }
+            if attrIds[idx] {
 				fmt.Println("attribute changed ", fieldTyp.Name)
 			}
-		}
+        }
 		idx++
 
 	}
 	return attrIds[:idx], nil
 }
 
-func (obj PolicyStmtConfig) MergeDbAndConfigObj(dbObj ConfigObj, attrSet []bool) (ConfigObj, error) {
+    func (obj PolicyStmtConfig) MergeDbAndConfigObj(dbObj ConfigObj, attrSet []bool) (ConfigObj, error) {
 	var mergedPolicyStmtConfig PolicyStmtConfig
 	objTyp := reflect.TypeOf(obj)
 	objVal := reflect.ValueOf(obj)
 	dbObjVal := reflect.ValueOf(dbObj)
 	mergedObjVal := reflect.ValueOf(&mergedPolicyStmtConfig)
 	idx := 0
-	for i := 0; i < objTyp.NumField(); i++ {
+	for i:=0; i<objTyp.NumField(); i++ {
 		if fieldTyp := objTyp.Field(i); fieldTyp.Anonymous {
 			continue
 		}
 
 		objField := objVal.Field(i)
 		dbObjField := dbObjVal.Field(i)
-		if attrSet[idx] {
+		if  attrSet[idx] {
 			if dbObjField.Kind() == reflect.Int ||
-				dbObjField.Kind() == reflect.Int8 ||
-				dbObjField.Kind() == reflect.Int16 ||
-				dbObjField.Kind() == reflect.Int32 ||
-				dbObjField.Kind() == reflect.Int64 {
+			   dbObjField.Kind() == reflect.Int8 ||
+			   dbObjField.Kind() == reflect.Int16 ||
+			   dbObjField.Kind() == reflect.Int32 ||
+			   dbObjField.Kind() == reflect.Int64 {
 				mergedObjVal.Elem().Field(i).SetInt(objField.Int())
 			} else if dbObjField.Kind() == reflect.Uint ||
-				dbObjField.Kind() == reflect.Uint8 ||
-				dbObjField.Kind() == reflect.Uint16 ||
-				dbObjField.Kind() == reflect.Uint32 ||
-				dbObjField.Kind() == reflect.Uint64 {
-				mergedObjVal.Elem().Field(i).SetUint(objField.Uint())
+			   dbObjField.Kind() == reflect.Uint8 ||
+			   dbObjField.Kind() == reflect.Uint16 ||
+			   dbObjField.Kind() == reflect.Uint32 ||
+			   dbObjField.Kind() == reflect.Uint64 {
+			    mergedObjVal.Elem().Field(i).SetUint(objField.Uint())
 			} else if dbObjField.Kind() == reflect.Bool {
-				mergedObjVal.Elem().Field(i).SetBool(objField.Bool())
+			    mergedObjVal.Elem().Field(i).SetBool(objField.Bool())
 			} else {
 				mergedObjVal.Elem().Field(i).SetString(objField.String())
 			}
 		} else {
 			if dbObjField.Kind() == reflect.Int ||
-				dbObjField.Kind() == reflect.Int8 ||
-				dbObjField.Kind() == reflect.Int16 ||
-				dbObjField.Kind() == reflect.Int32 ||
-				dbObjField.Kind() == reflect.Int64 {
+			   dbObjField.Kind() == reflect.Int8 ||
+			   dbObjField.Kind() == reflect.Int16 ||
+			   dbObjField.Kind() == reflect.Int32 ||
+			   dbObjField.Kind() == reflect.Int64 {
 				mergedObjVal.Elem().Field(i).SetInt(dbObjField.Int())
 			} else if dbObjField.Kind() == reflect.Uint ||
-				dbObjField.Kind() == reflect.Uint ||
-				dbObjField.Kind() == reflect.Uint8 ||
-				dbObjField.Kind() == reflect.Uint16 ||
-				dbObjField.Kind() == reflect.Uint32 {
-				mergedObjVal.Elem().Field(i).SetUint(dbObjField.Uint())
+			   dbObjField.Kind() == reflect.Uint ||
+			   dbObjField.Kind() == reflect.Uint8 ||
+			   dbObjField.Kind() == reflect.Uint16 ||
+			   dbObjField.Kind() == reflect.Uint32 {
+			    mergedObjVal.Elem().Field(i).SetUint(dbObjField.Uint())
 			} else if dbObjField.Kind() == reflect.Bool {
-				mergedObjVal.Elem().Field(i).SetBool(dbObjField.Bool())
+			    mergedObjVal.Elem().Field(i).SetBool(dbObjField.Bool())
+			} else if dbObjField.Kind() == reflect.Slice {
+				//slice
 			} else {
 				mergedObjVal.Elem().Field(i).SetString(dbObjField.String())
 			}
@@ -254,16 +291,16 @@ func (obj PolicyStmtConfig) MergeDbAndConfigObj(dbObj ConfigObj, attrSet []bool)
 	return mergedPolicyStmtConfig, nil
 }
 
-func (obj PolicyStmtConfig) UpdateObjectInDb(dbObj ConfigObj, attrSet []bool, dbHdl *sql.DB) error {
+    func (obj PolicyStmtConfig) UpdateObjectInDb(dbObj ConfigObj, attrSet []bool, dbHdl *sql.DB) error {
 	var fieldSqlStr string
 	dbPolicyStmtConfig := dbObj.(PolicyStmtConfig)
 	objKey, err := dbPolicyStmtConfig.GetKey()
 	objSqlKey, err := dbPolicyStmtConfig.GetSqlKeyStr(objKey)
 	dbCmd := "update " + "PolicyStmtConfig" + " set"
-	objTyp := reflect.TypeOf(obj)
+objTyp := reflect.TypeOf(obj)
 	objVal := reflect.ValueOf(obj)
 	idx := 0
-	for i := 0; i < objTyp.NumField(); i++ {
+	for i:=0; i<objTyp.NumField(); i++ {
 		if fieldTyp := objTyp.Field(i); fieldTyp.Anonymous {
 			continue
 		}
@@ -272,19 +309,19 @@ func (obj PolicyStmtConfig) UpdateObjectInDb(dbObj ConfigObj, attrSet []bool, db
 			fieldTyp := objTyp.Field(i)
 			fieldVal := objVal.Field(i)
 			if fieldVal.Kind() == reflect.Int ||
-				fieldVal.Kind() == reflect.Int8 ||
-				fieldVal.Kind() == reflect.Int16 ||
-				fieldVal.Kind() == reflect.Int32 ||
-				fieldVal.Kind() == reflect.Int64 {
+			   fieldVal.Kind() == reflect.Int8 ||
+			   fieldVal.Kind() == reflect.Int16 ||
+			   fieldVal.Kind() == reflect.Int32 ||
+			   fieldVal.Kind() == reflect.Int64 {
 				fieldSqlStr = fmt.Sprintf(" %s = '%d' ", fieldTyp.Name, int(fieldVal.Int()))
 			} else if fieldVal.Kind() == reflect.Uint ||
-				fieldVal.Kind() == reflect.Uint8 ||
-				fieldVal.Kind() == reflect.Uint16 ||
-				fieldVal.Kind() == reflect.Uint32 ||
-				fieldVal.Kind() == reflect.Uint64 {
-				fieldSqlStr = fmt.Sprintf(" %s = '%d' ", fieldTyp.Name, int(fieldVal.Uint()))
-			} else if fieldVal.Kind() == reflect.Bool {
-				fieldSqlStr = fmt.Sprintf(" %s = '%d' ", fieldTyp.Name, dbutils.ConvertBoolToInt(bool(fieldVal.Bool())))
+			   fieldVal.Kind() == reflect.Uint8 ||
+			   fieldVal.Kind() == reflect.Uint16 ||
+			   fieldVal.Kind() == reflect.Uint32 ||
+			   fieldVal.Kind() == reflect.Uint64 {
+			    fieldSqlStr = fmt.Sprintf(" %s = '%d' ", fieldTyp.Name, int(fieldVal.Uint()))
+			} else if objVal.Kind() == reflect.Bool {
+			    fieldSqlStr = fmt.Sprintf(" %s = '%d' ", fieldTyp.Name, dbutils.ConvertBoolToInt(bool(fieldVal.Bool())))
 			} else {
 				fieldSqlStr = fmt.Sprintf(" %s = '%s' ", fieldTyp.Name, fieldVal.String())
 			}
