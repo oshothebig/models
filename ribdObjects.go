d301b776a84000d3a74d19cee8947852dab2c16c
mjoshiSnaproute <mjoshi@Snaproute.com>
2016-04-20 10:39:01 -0700

    modified:   ribdObjects.go

--- ribdObjects.go
+++ ribdObjects.go
@@ -45,25 +45,6 @@ type PolicyConditionState struct {
 	PolicyStmtList []string `DESCRIPTION: "List of policy statements using this condition"`
 }
 
-/*type PolicyAction struct {
-	BaseObj
-	Name                           string `SNAPROUTE: "KEY", ACCESS:"w", MULTIPLICITY:"*", DESCRIPTION: "PolicyActionName"`
-	ActionType                     string `DESCRIPTION: "Specifies the type of the action  - eg: RouteDisposition/NetworkStatementAdvertise/Redistribution/SetAdminDistance"`
-	SetAdminDistanceValue          int32  `DESCRIPTION :"Specifies the value of the admin distance/protocol preference when the action type is SetAdminDistance"`
-	Accept                         bool   `DESCRIPTION :"When set to true, along with action type RouteDisposition, indicates to accept the policy entity"`
-	Reject                         bool   `DESCRIPTION :"When set to true, along with action type RouteDisposition, indicates to reject the policy entity"`
-	RedistributeAction             string `DESCRIPTION :"Used in conjuction with RedistributeTargetProtocol for action type Redistribute, indicates to allow/block redistribution"`
-	RedistributeTargetProtocol     string `DESCRIPTION :"Used in conjuction with RedistributeAction for action type Redistribute, indicates the target protocol for redistribution"`
-	NetworkStatementTargetProtocol string `DESCRIPTION :"Used for action type NetworkStatementAdvertise, indicates the target protocol for Network Statement Advertisement"`
-}
-
-type PolicyActionState struct {
-	BaseObj
-	Name           string `SNAPROUTE: "KEY", ACCESS:"r", MULTIPLICITY:"*", DESCRIPTION: "Action name"`
-	ActionInfo     string
-	PolicyStmtList []string `DESCRIPTION: "List of policy statements using this condition"`
-}
-*/
 type PolicyStmt struct {
 	BaseObj
 	Name            string   `SNAPROUTE: "KEY", ACCESS:"w", MULTIPLICITY:"*", DESCRIPTION: "Policy Statement Name"`
@@ -88,7 +69,7 @@ type PolicyDefinition struct {
 	Name          string                           `SNAPROUTE: "KEY", ACCESS:"w", MULTIPLICITY:"*", DESCRIPTION: "Policy Name"`
 	Priority      int32                            `DESCRIPTION :"Priority of the policy w.r.t other policies configured",RANGE:0-255`
 	MatchType     string                           `DESCRIPTION :"Specifies whether to match all/any of the statements within this policy",SELECTION:"all"/"any",DEFAULT:"all"`
-	StatementList []PolicyDefinitionStmtPriority `DESCRIPTION :"Specifies list of statements along with their precedence order."`
+	StatementList []PolicyDefinitionStmtPriority   `DESCRIPTION :"Specifies list of statements along with their precedence order."`
 }
 type PolicyDefinitionState struct {
 	BaseObj
