53889f2b97a033eabf4f730d03d2cf272b1c446a
mjoshiSnaproute <mjoshi@Snaproute.com>
2016-05-06 11:17:40 -0700

    modified:   ribdObjects.go

--- ribdObjects.go
+++ ribdObjects.go
@@ -1,33 +1,23 @@
 package models
 
 type NextHopInfo struct {
-	NextHopIp          string   `DESCRIPTION: "next hop ip of the route"`
+	NextHopIp          string   `DESCRIPTION: "next hop ip of the route, DEFAULT:"0.0.0.0""`
 	OutgoingIntfType   string   `DESCRIPTION :"Interface type of the next hop interface"`
 	OutgoingInterface  string   `DESCRIPTION :"Interface ID of the next hop interface"`
-	Protocol           string   `DESCRIPTION :"Protocol type of the route"`
+	Protocol           string   `DESCRIPTION :"Protocol type of the route",DEFAULT:"STATIC"`
+	Weight             int32    `DESCRIPTION : "Weight of the next hop",DEFAULT:0, RANGE:0-31`
 }
 type IPv4Route struct {
 	ConfigObj
-	DestinationNw     string `SNAPROUTE: "KEY", ACCESS:"w", MULTIPLICITY:"*", ACCELERATED: "true", DESCRIPTION: "IP address of the route"`
-	NetworkMask       string `SNAPROUTE: "KEY", ACCESS:"w", MULTIPLICITY:"*", ACCELERATED: "true", DESCRIPTION: "mask of the route"`
-	NextHopIp         string `SNAPROUTE: "KEY", ACCESS:"w", MULTIPLICITY:"*", ACCELERATED: "true", DESCRIPTION: "next hop ip of the route"`
+	DestinationNw     string `SNAPROUTE: "KEY", ACCESS:"w", MULTIPLICITY:"*", ACCELERATED: "true", DESCRIPTION: "IP address of the route, can be specified either in CIDR notation or as a IP address."`
+	NetworkMask       string `SNAPROUTE: "KEY", ACCESS:"w", MULTIPLICITY:"*", ACCELERATED: "true", DESCRIPTION: "mask of the route, required to be non-zero in case DestinationNw is a non-CIDR address.", DEFAULT:"255.255.255.255"`
+	NextHopIp         string `SNAPROUTE: "KEY", ACCESS:"w", MULTIPLICITY:"*", ACCELERATED: "true", DESCRIPTION: "next hop ip of the route", DEFAULT:"0.0.0.0"`
 	Cost              uint32 `DESCRIPTION :"Cost of this route", DEFAULT:0`
 	OutgoingIntfType  string `DESCRIPTION :"Interface type of the next hop interface"`
 	OutgoingInterface string `DESCRIPTION :"Interface ID of the next hop interface"`
-	Protocol           string   `DESCRIPTION :"Protocol type of the route"`
+	Protocol           string   `DESCRIPTION :"Protocol type of the route", DEFAULT:"STATIC"`
+	Weight             int32    `DESCRIPTION : "Weight of the next hop", DEFAULT:0, RANGE:0-31`
 }
-/*type IPv4RouteState struct {
-	ConfigObj
-	DestinationNw      string   `SNAPROUTE: "KEY", ACCESS:"r", MULTIPLICITY:"*", DESCRIPTION: "IP address of the route", USESTATEDB:"true"`
-	NextHopIp          string   `SNAPROUTE: "KEY", ACCESS:"r", MULTIPLICITY:"*", DESCRIPTION: "next hop ip of the route"`
-	OutgoingIntfType   string   `DESCRIPTION :"Interface type of the next hop interface"`
-	OutgoingInterface  string   `DESCRIPTION :"Interface ID of the next hop interface"`
-	Protocol           string   `DESCRIPTION :"Protocol type of the route"`
-	PolicyList         []string `DESCRIPTION :"List of policies applied on this route"`
-	IsNetworkReachable bool     `DESCRIPTION :"Indicates whether this network is reachable"`
-	RouteCreatedTime   string   `DESCRIPTION :"Time when the route was added"`
-	RouteUpdatedTime   string   `DESCRIPTION :"Time when the route was last updated"`
-}*/
 /*type IPv4Route struct {
 	ConfigObj
 	DestinationNw     string `SNAPROUTE: "KEY", ACCESS:"w", MULTIPLICITY:"*", ACCELERATED: "true", DESCRIPTION: "IP address of the route"`
