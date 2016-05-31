11f97353deaa6acc45cd94f06c4399844a0f4599
Avoy Nanda <ananda@snaproute.com>
2016-05-31 11:26:29 -0700

    Moved objects supported by confd to localObjects

--- objects.go
+++ objects.go
@@ -24,8 +24,6 @@
 package models
 
 import (
-	"encoding/json"
-	"fmt"
 	"github.com/garyburd/redigo/redis"
 )
 
@@ -78,63 +76,4 @@ func (obj baseObj) GetAllObjFromDb(dbHdl redis.Conn) ([]ConfigObj, error) {
 }
 func (obj baseObj) GetBulkObjFromDb(startIndex int64, count int64, dbHdl redis.Conn) (error, int64, int64, bool, []ConfigObj) {
 	return nil, 0, 0, false, nil
-}
-
-type SystemStatusState struct {
-	baseObj
-	Name           string        `SNAPROUTE: "KEY", ACCESS:"r",  MULTIPLICITY:"1", DESCRIPTION: "Name of the system"`
-	Ready          bool          `DESCRIPTION: "System is ready to accept api calls"`
-	Reason         string        `DESCRIPTION: "Reason if system not ready"`
-	UpTime         string        `DESCRIPTION: "Uptime of this system"`
-	NumCreateCalls string        `DESCRIPTION: "Number of create api calls made"`
-	NumDeleteCalls string        `DESCRIPTION: "Number of delete api calls made"`
-	NumUpdateCalls string        `DESCRIPTION: "Number of update api calls made"`
-	NumGetCalls    string        `DESCRIPTION: "Number of get api calls made"`
-	NumActionCalls string        `DESCRIPTION: "Number of action api calls made"`
-	FlexDaemons    []DaemonState `DESCRIPTION: "Daemon states"`
-}
-
-func (obj SystemStatusState) UnmarshalObject(body []byte) (ConfigObj, error) {
-	var systemStatus SystemStatusState
-	var err error
-	if len(body) > 0 {
-		if err = json.Unmarshal(body, &systemStatus); err != nil {
-			fmt.Println("### Trouble in unmarshaling SystemStatus from Json", body)
-		}
-	}
-
-	return systemStatus, err
-}
-
-func (obj SystemStatusState) GetKey() string {
-	return ""
-}
-
-type RepoInfo struct {
-	Name   string `DESCRIPTION: "Name of the git repo"`
-	Sha1   string `DESCRIPTION: "Git commit Sha1"`
-	Branch string `DESCRIPTION: "Branch name"`
-	Time   string `DESCRIPTION: "Build time"`
-}
-
-type SystemSwVersionState struct {
-	baseObj
-	FlexswitchVersion string     `SNAPROUTE: "KEY", ACCESS:"r",  MULTIPLICITY:"1", DESCRIPTION: "Flexswitch version"`
-	Repos             []RepoInfo `DESCRIPTION: "Git repo details"`
-}
-
-func (obj SystemSwVersionState) UnmarshalObject(body []byte) (ConfigObj, error) {
-	var systemSwVersion SystemSwVersionState
-	var err error
-	if len(body) > 0 {
-		if err = json.Unmarshal(body, &systemSwVersion); err != nil {
-			fmt.Println("### Trouble in unmarshaling SystemSwVersion from Json", body)
-		}
-	}
-
-	return systemSwVersion, err
-}
-
-func (obj SystemSwVersionState) GetKey() string {
-	return ""
 }
