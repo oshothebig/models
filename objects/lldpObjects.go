285704ab901f9beadc3a33f6604c9fca4db96ea5
Avoy Nanda <ananda@snaproute.com>
2016-07-28 23:44:30 -0700

    fix for lldp intf discovery

--- objects/lldpObjects.go
+++ objects/lldpObjects.go
@@ -31,13 +31,13 @@ type LLDPGlobal struct {
 
 type LLDPIntf struct {
 	baseObj
-	IfIndex int32 `SNAPROUTE: "KEY", ACCESS:"w", MULTIPLICITY:"1",DESCRIPTION: "IfIndex where lldp needs is enabled/disabled", AUTODISCOVER:"true"`
+	IfIndex int32 `SNAPROUTE: "KEY", ACCESS:"rw", MULTIPLICITY:"*",DESCRIPTION: "IfIndex where lldp needs is enabled/disabled", AUTODISCOVER:"true"`
 	Enable  bool  `DESCRIPTION: "Enable/Disable lldp config Per Port", DEFAULT:"true"`
 }
 
 type LLDPIntfState struct {
 	baseObj
-	IfIndex             int32  `SNAPROUTE: "KEY", ACCESS:"r", MULTIPLICITY:"1",DESCRIPTION: "IfIndex where lldp needs to be configured"`
+	IfIndex             int32  `SNAPROUTE: "KEY", ACCESS:"r", MULTIPLICITY:"*",DESCRIPTION: "IfIndex where lldp needs to be configured"`
 	Enable              bool   `DESCRIPTION: "Enable/Disable lldp config"`
 	LocalPort           string `DESCRIPTION: "Local interface"`
 	PeerMac             string `DESCRIPTION: "Mac address of direct connection"`
