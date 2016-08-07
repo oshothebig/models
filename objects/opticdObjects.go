9f4ed6b5cdb4889e670266380f19959681313808
Vikram Ragukumar <vragukumar@snaproute.com>
2016-08-06 21:50:26 -0700

    Models changes for opticd

--- objects/opticdObjects.go
+++ objects/opticdObjects.go
@@ -53,7 +53,7 @@ type DWDMModuleNwIntf struct {
 	ModulationFmt           string  `DESCRIPTION: "Modulation format to use for this network interface", SELECTION: "QPSK"/"8QAM/"16QAM", DEFAULT:"16QAM"`
 	TxPower                 float64 `DESCRIPTION: "Transmit output power for this network interface in dBm, MIN:0, MAX:4294967295", DEFAULT:0`
 	WaveLength              uint16  `DESCRIPTION: "The ITU-T G.694.1 grid wavelength value to use for this network interface in nm", MIN:1530, MAX:1565`
-	FECMode                 string  `DESCRIPTION: "DWDM Module network interface FEC mode", SELECTION: "15%SDFEC"/"15OvrHeadSDFEC%/25%OvrHeadSDFEC"`
+	FECMode                 string  `DESCRIPTION: "DWDM Module network interface FEC mode", SELECTION: "15%SDFEC"/"15%OvrHeadSDFEC/25%OvrHeadSDFEC"`
 	DiffEncoding            bool    `DESCRIPTION: "Control to enable/disable DWDM Module network interface encoding type", DEFAULT: true`
 	TxPulseShapeFltrType    string  `DESCRIPTION: "TX pulse shaping filter type", SELECTION: "RootRaisedCos"/"RaisedCos"/"Gaussian", DEFAULT:RootRaisedCos"`
 	TxPulseShapeFltrRollOff float64 `DESCRIPTION: "TX pulse shape filter roll off factor, MIN:0.004, MAX:1.0", DEFAULT:0.301`
@@ -61,16 +61,23 @@ type DWDMModuleNwIntf struct {
 
 type DWDMModuleNwIntfState struct {
 	baseObj
-	ModuleId          uint8  `SNAPROUTE: "KEY", ACCESS:"r", MULTIPLICITY: "*", DESCRIPTION: "DWDM Module identifier"`
-	NwIntfId          uint8  `SNAPROUTE: "KEY", DESCRIPTION: "DWDM Module network interface identifier"`
-	TxChanGridSpacing string `DESCRIPTION: "The channel grid spacing used for this network interface in GHz"`
+	ModuleId                        uint8   `SNAPROUTE: "KEY", ACCESS:"r", MULTIPLICITY: "*", DESCRIPTION: "DWDM Module identifier"`
+	NwIntfId                        uint8   `SNAPROUTE: "KEY", DESCRIPTION: "DWDM Module network interface identifier"`
+	TxChanGridSpacing               string  `DESCRIPTION: "The channel grid spacing used for this network interface in GHz"`
+	CurrentBER                      float64 `DESCRIPTION: "Current value of BER on the DWDM module network interface"`
+	MinBEROverPMInterval            float64 `DESCRIPTION: "Minimum value of BER over the last PM interval for the DWDM module network interface"`
+	AvgBEROverPMInterval            float64 `DESCRIPTION: "Average value of BER over the last PM interval for the DWDM module network interface"`
+	MaxBEROverPMInterval            float64 `DESCRIPTION: "Maximum value of BER over the last PM interval for the DWDM module network interface"`
+	CurrUncorrectableFECBlkCnt      float64 `DESCRIPTION: "Current value of uncorrectable FEC code block count"`
+	UncorrectableFECBlkCntOverPMInt float64 `DESCRIPTION: "Average value of uncorrectable FEC code block count over the last PM interval"`
 }
 
 type DWDMModuleClntIntf struct {
 	baseObj
 	ModuleId                  uint8 `SNAPROUTE: "KEY", ACCESS:"rw", MULTIPLICITY: "*", DESCRIPTION: "DWDM Module identifier"`
 	ClntIntfId                uint8 `SNAPROUTE: "KEY", DESCRIPTION: "DWDM Module client interface identifier"`
-	FECEnabled                bool  `DESCRIPTION: "802.3bj FEC enable/disable state", DEFAULT: false`
+	TXFECDecDisable           bool  `DESCRIPTION: "802.3bj FEC decoder enable/disable state for traffic from Host to DWDM Module", DEFAULT: false`
+	RXFECDecDisable           bool  `DESCRIPTION: "802.3bj FEC decoder enable/disable state for traffic from DWDM module to Host", DEFAULT: false`
 	HostTxEqLfCtle            uint8 `DESCRIPTION: "Host interface TX deserializer equalization. LELPZRC LF-CTLE LFPZ gain code.", MIN:0, MAX:8, DEFAULT:0`
 	HostTxEqCtle              uint8 `DESCRIPTION: "Host interface TX deserializer equalization. LELRC CTLE LE gain code.", MIN:0, MAX:20, DEFAULT:18`
 	HostTxEqDfe               uint8 `DESCRIPTION: "Host interface TX deserializer equalization. s-DFE, DFE tap coefficient", MIN:0, MAX:63, DEFAULT:0`
