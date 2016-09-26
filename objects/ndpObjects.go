e7a2c845e5a659912b6a16b2a559e4a72b2dbb48
jgheewala <jgheewala@snaproute.com>
2016-09-25 18:28:37 -0700

    ndp interface level state object

--- objects/ndpObjects.go
+++ objects/ndpObjects.go
@@ -45,12 +45,29 @@ type NDPGlobalState struct {
 
 type NDPEntryState struct {
 	baseObj
-	IpAddr          string `SNAPROUTE: "KEY", ACCESS:"r", MULTIPLICITY:"*", DESCRIPTION: "Neighbor's IP Address"`
+	IpAddr  string `SNAPROUTE: "KEY", ACCESS:"r", MULTIPLICITY:"*", DESCRIPTION: "Neighbor's IP Address"`
+	MacAddr string `DESCRIPTION: "MAC address of the neighbor machine with corresponding IP Address"`
+	Vlan    string `DESCRIPTION: "Vlan ID of the Router Interface to which neighbor is attached to"`
+	Intf    string `DESCRIPTION: "Router Interface to which neighbor is attached to"`
+	IfIndex int32  `DESCRIPTION: "ifIndex where neighbor is learned"`
+}
+
+type NeighborEntry struct {
+	IpAddr          string `DESCRIPTION: "Neighbor's IP Address"`
 	MacAddr         string `DESCRIPTION: "MAC address of the neighbor machine with corresponding IP Address"`
-	Vlan            string `DESCRIPTION: "Vlan ID of the Router Interface to which neighbor is attached to"`
-	Intf            string `DESCRIPTION: "Router Interface to which neighbor is attached to"`
-	IfIndex         int32  `DESCRIPTION: "ifIndex where neighbor is learned"`
 	ExpiryTimeLeft  string `DESCRIPTION: "Time left before entry expires in case neighbor departs"`
 	SendPackets     int64  `DESCRIPTION: "Total Packets send to the neighbor"`
 	ReceivedPackets int64  `DESCRIPTION: "Total Packets received from neighbor"`
+	State           string `DESCRIPTION: "Reachablity Information about the neighbor"`
+}
+
+type NDPIntfState struct {
+	baseObj
+	IntfRef         string `SNAPROUTE: "KEY", ACCESS:"r", MULTIPLICITY:"*", DESCRIPTION: "Port where neighbor ip's are learned"`
+	IfIndex         int32  `DESCRIPTION: "System generated unique id for local port"`
+	LinkScopeIp     string `DESCRIPTION: "Local Port link scope ip address`
+	GlobalScopeIp   string `DESCRIPTION: "Local Port Global Scope ip address"`
+	SendPackets     int64  `DESCRIPTION: "Total Packets send from the local port"`
+	ReceivedPackets int64  `DESCRIPTION: "Total Packets received by local port"`
+	Neighbors       []NeighborEntry
 }
