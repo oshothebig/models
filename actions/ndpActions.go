90a2bd995c5d7e98cab2368164c7499c550c7bf7
jgheewala <jgheewala@snaproute.com>
2016-10-11 07:20:02 -0700

    ndp actions object

---
+++ actions/ndpActions.go
@@ -0,0 +1,44 @@
+//
+//Copyright [2016] [SnapRoute Inc]
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//     Unless required by applicable law or agreed to in writing, software
+//     distributed under the License is distributed on an "AS IS" BASIS,
+//     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//     See the License for the specific language governing permissions and
+//     limitations under the License.
+//
+// _______  __       __________   ___      _______.____    __    ____  __  .___________.  ______  __    __
+// |   ____||  |     |   ____\  \ /  /     /       |\   \  /  \  /   / |  | |           | /      ||  |  |  |
+// |  |__   |  |     |  |__   \  V  /     |   (----` \   \/    \/   /  |  | `---|  |----`|  ,----'|  |__|  |
+// |   __|  |  |     |   __|   >   <       \   \      \            /   |  |     |  |     |  |     |   __   |
+// |  |     |  `----.|  |____ /  .  \  .----)   |      \    /\    /    |  |     |  |     |  `----.|  |  |  |
+// |__|     |_______||_______/__/ \__\ |_______/        \__/  \__/     |__|     |__|      \______||__|  |__|
+//
+
+package actions
+
+type NdpDeleteByIfName struct {
+	baseAction
+	IfName string `DESCRIPTION: "All the NDP learned for end host on given L3 interface will be deleted"`
+}
+
+type NdpDeleteByIPv6Addr struct {
+	baseAction
+	IpAddr string `DESCRIPTION: "End Host IPV6 Address for which corresponding NDP entry needs to be deleted"`
+}
+
+type NdpRefreshByIfName struct {
+	baseAction
+	IfName string `DESCRIPTION: "All the NDP learned on given L3 interface will be re-learned"`
+}
+
+type NdpRefreshByIPv6Addr struct {
+	baseAction
+	IpAddr string `DESCRIPTION: "Neighbor's IPV6 Address for which corresponding NDP entry needs to be re-learned"`
+}
